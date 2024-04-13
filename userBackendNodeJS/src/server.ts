ba7eef46330acb26aa99e94712762b1cd0e70ada
thiloschenkelberg <thiloschenkelberg@gmail.com>
2024-04-14 01:18:17 +0200

    Fix userBackend dotenv path

--- userBackendNodeJS/src/server.ts
+++ userBackendNodeJS/src/server.ts
@@ -3,7 +3,7 @@ import userRouter from "./controllers/user.controller-mongodb"
 import cors from "cors"
 import dotenv from "dotenv"
 
-dotenv.config(({ path: '../.env'}))
+dotenv.config(({ path: '../../.env'}))
 
 const app = express()
 const port = process.env.PORT || 8080 // Set the desired port number
