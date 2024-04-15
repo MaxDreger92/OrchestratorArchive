faf534fd720f4a041262017ebfef038d8d86aa6b
thiloschenkelberg <thiloschenkelberg@gmail.com>
2024-04-15 10:27:55 +0000

    Remove bad urls

--- mat2devplatform/urls.py
+++ mat2devplatform/urls.py
@@ -38,8 +38,8 @@ spa_view = never_cache(TemplateView.as_view(template_name='index.html'))
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('default.urls')),
-    path('', include('matgraph.urls')),
-    path('', include('usermanagement.urls')),
+    #path('', include('matgraph.urls')),
+    #path('', include('usermanagement.urls')),
     path('', include('matching.urls')),
     path('', include('importing.urls')),
     # Catch-all for SPA
