7dff1ed0fc762ac5665b736b84db00ca5ef09481
Thilo Schenkelberg <thiloschenkelberg@gmail.com>
2024-08-27 14:03:44 +0200

    Initial push

--- mat2devplatform/settings.py
+++ mat2devplatform/settings.py
@@ -116,7 +116,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
 ROOT_URLCONF = 'mat2devplatform.urls'
 # OpenAI API Key
 TEMPLATES = [
--- matching/urls.py
+++ matching/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import workflow_matcher_view, workflow_matcher
+from .views import workflow_matcher
 
 urlpatterns = [
-    # path('search/', workflow_matcher_view, name='matcher'),
-    path('api/match/fabrication-workflow/', workflow_matcher, name='fabrication_workflow'),
+    path('api/match/fabrication-workflow', workflow_matcher, name='fabrication_workflow'),
 ]
